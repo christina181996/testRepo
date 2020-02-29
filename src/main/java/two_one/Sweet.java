c1d2f4ae10418986009f90b21809300aaf075b94
Ham <hamazasp.torosyan@gmail.com>
2020-03-01 02:23:00 +0400

    Lombok EqualsAndHashes

--- src/main/java/two_one/Sweet.java
+++ src/main/java/two_one/Sweet.java
@@ -1,12 +1,12 @@
 package two_one;
 
-import java.util.Objects;
-
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 abstract class Sweet implements Comparable<Sweet> {
     private String brand;
     private int price;
@@ -23,25 +23,5 @@ abstract class Sweet implements Comparable<Sweet> {
     @Override
     public int compareTo(Sweet o) {
         return this.price - o.price;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Sweet)) {
-            return false;
-        }
-        Sweet sweet = (Sweet) o;
-        return price == sweet.price &&
-            weight == sweet.weight &&
-            brand.equals(sweet.brand) &&
-            type.equals(sweet.type);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(brand, price, weight, type);
     }
 }
